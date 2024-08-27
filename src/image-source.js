059eaaabb71b3c5d7c25a87def3ef653b7631c7c
Mohsin-Shafique <164845524+Mohsin-Shafique@users.noreply.github.com>
2024-08-27 16:05:28 +0500

    no message

--- src/image-source.js
+++ src/image-source.js
@@ -18,19 +18,26 @@ const onSourceSelect = () => {
   });
 };
 
-const onWebcamSelect = (e) => {
-  console.log("Webcam button clicked");
+const onWebcamSelect = async (e) => {
   try {
-    const source = getSource(e.target.value);
-    console.log("Source:", source);
-    if (source) {
+    console.log("Webcam button clicked"); // Debugging
+    const constraints = {
+      video: true, // Request video stream
+      audio: false, // Disable audio
+    };
+
+    // Check if media devices are available
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      const source = getSource(e.target.value, stream);
+      console.log("Source:", source); // Debugging
       startPlayer(source);
       onSourceSelect();
     } else {
-      console.error("Failed to get webcam source");
+      console.error("Media devices are not supported.");
     }
   } catch (error) {
-    console.error("Error selecting webcam source:", error);
+    console.error("Error accessing webcam:", error);
   }
 };
 
